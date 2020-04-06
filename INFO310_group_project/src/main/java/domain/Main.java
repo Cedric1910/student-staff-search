7eee70a4da7e81501c0b4315f006d2027bb800de
Hugo <hugonzb@gmail.com>
2020-04-07 10:55:18 +1200

    Fixed up domain

--- INFO310_group_project/src/main/java/domain/Main.java
+++ INFO310_group_project/src/main/java/domain/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package INFO310_group_project;
+package domain;
 
 /**
  *
---
+++ INFO310_group_project/src/main/java/domain/ResearchProject.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author cedricstephani
+ */
+public class ResearchProject {
+    int projectID; 
+    String studentID; 
+    String staffID; 
+    String projectName; 
+    String description; 
+    String category; 
+    String subCategory; 
+    String status; 
+
+    public ResearchProject(int projectID, String studentID, String staffID, String projectName, String description, String category, String subCategory, String status) {
+        this.projectID = projectID;
+        this.studentID = studentID;
+        this.staffID = staffID;
+        this.projectName = projectName;
+        this.description = description;
+        this.category = category;
+        this.subCategory = subCategory;
+        this.status = status;
+    }
+
+    public int getProjectID() {
+        return projectID;
+    }
+
+    public void setProjectID(int projectID) {
+        this.projectID = projectID;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(String subCategory) {
+        this.subCategory = subCategory;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
