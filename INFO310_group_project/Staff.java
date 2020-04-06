d59f4763b4d48e957f16a49e54103387bfade63c
Cedric Stephani <stece850@student.otago.ac.nz>
2020-04-06 16:55:11 +1200

    Added domain files matching the current ERD

---
+++ INFO310_group_project/Staff.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.util.List;
+
+/**
+ *
+ * @author cedricstephani
+ */
+public class Staff {
+    String id; //Just their otago staff ID to keep it simple. 
+    String firstName; 
+    String surname; 
+    String username; 
+    String password; 
+    String email; 
+    List<String> categories;
+    List<String> subCategories;
+    boolean availability; 
+
+    public Staff(String id, String firstName, String surname, String username, String password, String email, List<String> categories, List<String> subCategories, boolean availability) {
+        this.id = id;
+        this.firstName = firstName;
+        this.surname = surname;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.categories = categories;
+        this.subCategories = subCategories;
+        this.availability = availability;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    public List<String> getSubCategories() {
+        return subCategories;
+    }
+
+    public void setSubCategories(List<String> subCategories) {
+        this.subCategories = subCategories;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+    
+    
+    
+}
