3a3c7e0b5cdbd888470e60b34ee6e48ad25dc5af
Leon <le.o.n@outlook.com>
2020-04-08 09:19:36 +1200

    I think I've finished work on the schema.sql, haven't tested it yet to see if it's error free but I'm sure it's fine :)

--- INFO310_group_project/src/main/resources/schema.sql
+++ INFO310_group_project/src/main/resources/schema.sql
@@ -1,5 +1,36 @@
-create table ResearchProject();
+/*
+  Can easily alter the varchar sizes if need be.
+*/
 
-create table Staff();
+create table ResearchProject(
+       projectID varchar(15),
+       studentID varchar(15),
+       projectname varchar(50),
+       description varchar(200),
+       category varchar(50),
+       subcategory varchar(75),
+       status varchar(35),
+       constraint project_PK primary key (projectID)
+);
 
-create table Student();
+create table Staff(
+       id varchar(15),
+       firstname varchar(30),
+       surname varchar(40),
+       username varchar(40),
+       password varchar(40),
+       email varchar(60),
+       availability boolean, /* Pretty sure that boolean is a thing in h2, will have to see */
+       constraint staff_PK primary key (id)
+);
+
+create table Student(
+       id varchar(15),
+       firstname varchar(30),
+       surname varchar(40),
+       username varchar(40),
+       password varchar(40),
+       email varchar(60),
+       searching boolean,
+       constraint student_PK primary key (id)
+);
