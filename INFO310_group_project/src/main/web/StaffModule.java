9e7d8b4d595fc0a216f68e6350914611bc1cdb80
Leon <le.o.n@outlook.com>
2020-04-09 15:18:25 +1200

    Started working on StaffModule.java

--- INFO310_group_project/src/main/web/StaffModule.java
+++ INFO310_group_project/src/main/web/StaffModule.java
@@ -6,3 +6,12 @@ import org.jooby.Result;
 import org.jooby.Status;
 import domain.Staff;
 
+public class StaffModule extends Jooby{
+    // StaffDAO staffDAO = new StaffDAO(); This was in the 202 file for CustomerModule
+    // From Hugo's github, not entirely sure it needs to be here
+
+    public StaffModule(StaffDAO staffDAO){
+	port(8080);
+	get("/api/")
+    }
+}
