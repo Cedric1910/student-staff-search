85b3940bd28fce09015ac0fdd1e49978c0efa77c
Leon <le.o.n@outlook.com>
2020-04-09 12:36:23 +1200

    Finished AssetModule.java

---
+++ INFO310_group_project/src/main/web/AssetModule.java
@@ -0,0 +1,26 @@
+package web;
+
+import org.jooby.Jooby;
+import org.jooby.Results;
+
+/**
+ * This class is similar to the AssetModule class from INFO202 S2 2019
+ * I'd be lying if I said I fully understood what this class is doing, however I'm
+ * pretty sure that it is setting up the directories for the web server or something.
+
+ * @author Leon Hoogenraad
+ */
+public class AssetModule{
+    public AssetModule(){
+	assets("/*.html");
+	assets("/css/*.css");
+	assets("/js/*.js");
+	assets("/images/*.png");
+	assets("/images/*.jpg");
+
+	//Here we're setting index.html as our home page
+	assets("/", "index.html");
+	//This prevents a 404 error due to browsers requesting favicons apparently
+	get("/favicon", () -> Results.noContext());
+    }
+}
