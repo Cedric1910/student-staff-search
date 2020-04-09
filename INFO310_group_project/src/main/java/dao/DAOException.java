b697f29de1c24ba375e924d462884415b3fce11f
Hugo <hugonzb@gmail.com>
2020-04-09 15:39:16 +1200

    DAO Exception class completed.

--- INFO310_group_project/src/main/java/dao/DAOException.java
+++ INFO310_group_project/src/main/java/dao/DAOException.java
@@ -6,10 +6,15 @@ package dao;
  * @author Hugo Baird
  */
 
+/**
+ * Forwards DAO exception/Run Time exceptions to console
+ * when errors occur during database access.
+ */
 public class DAOException extends RuntimeException {
     public DAOException(String reason) {
         super(reason);
     }
+    
     public DAOException(String reason, Throwable cause) {
         super(reason, cause);
     }
