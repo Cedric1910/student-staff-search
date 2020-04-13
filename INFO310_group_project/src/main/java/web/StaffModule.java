06e9f356b59ab6a961ac070c40202c9ed61e2ac5
Hugo <hugonzb@gmail.com>
2020-04-13 15:59:02 +1200

    Fixed Staff Module methods and added correct imports.

--- INFO310_group_project/src/main/java/web/StaffModule.java
+++ INFO310_group_project/src/main/java/web/StaffModule.java
@@ -1,24 +1,26 @@
 package web;
 
 import dao.StaffDAO;
+import dao.StaffInterface;
 import org.jooby.Jooby;
 import org.jooby.Status;
 import domain.Staff;
 
-public class StaffModule extends Jooby{
-    // StaffDAO staffDAO = new StaffDAO(); This was in the 202 file for CustomerModule
-    // From Hugo's github, not entirely sure it needs to be here
+public class StaffModule extends Jooby {
+    
+    StaffInterface StaffDAO = new StaffDAO();
 
-    public StaffModule(StaffDAO staffDAO){
+    public StaffModule(StaffInterface StaffDAO) {
 	port(8080);
-	/* Not sure if this path /api/register/staff will work, I'm assuming you set the path here? */
+        
+        // Returns a list of all available staff categories
+        get("/api/categories/", () -> StaffDAO.returnAvailableCategories());
+        
+	// Calls staff DAO to add new staff account to the database
 	post("/api/register/staff", (req, rsp) -> {
 		Staff staff = req.body().to(Staff.class);
-		staffDAO.saveStaff(staff);
+		StaffDAO.saveStaff(staff);
 		rsp.status(Status.CREATED);
 	    });
-	//get("/api/staff/:categories", (req, rsp) -> {
-	//	return staffDAO.returnAvailableCategories();
-	//});
     }
 }
