fe21aa6278a9adf86df4481c97611b7273fbd2c3
Hugo <hugonzb@gmail.com>
2020-05-14 10:34:01 +1200

    Added proper sql default data, need to add more
    reference #43

--- INFO310_group_project/src/main/resources/schema.sql
+++ INFO310_group_project/src/main/resources/schema.sql
@@ -57,22 +57,30 @@ create table ContactRequest(
  * IF YOU ADD A NEW ACCOUNT TO THIS, MAKE SURE USERNAME IS ALL LOWERCASE. 
  */
 insert into Student (studentID, firstname, surname, dateOfBirth, gender, username, password, email,phoneNumber,description,address, category, searching) 
-values (1143134, 'Leon', 'Hoogenraad','20/05/1998','male', 'lhoogenraad', 'password', 'le.o.n@outlook.com','021026111321','I am here to search for a research project partner','16 some street dunedin','Computer Science', false);
+values (1143134, 'Leon', 'Hoogenraad','20/05/1998','Male', 'lhoogenraad', 'password', 'le.o.n@outlook.com','(+64)021 0261 1321','Hi there, I am looking for a lecturer to pair with for my PhD.','16 Some Street, Dunedin','Computer Science', false);
 insert into Student (studentID, firstname, surname, dateOfBirth, gender, username, password, email,phoneNumber,description,address, category, searching)  
-values (2423423, 'Hugo', 'Baird','15/03/1999','male', 'hbaird', 'password','hugo@outlook.com','02102788182','I am here to search for a research project partner','153 park street dunedin','Computer Science', false);
+values (2423423, 'Hugo', 'Baird','12/03/1999','Male', 'hbaird', 'password','hugo.baird@gmail.com','(+64)021 0812 0097','I am here to find a supportive staff member to help me complete my post-graduate research.','153 Park Street, dunedin','Computer Science', false);
 insert into Student (studentID, firstname, surname, dateOfBirth, gender, username, password, email,phoneNumber,description,address, category, searching)  
-values (3345345, 'Julia', 'McDowell','17/05/1999','female', 'jmcdowell', 'password', 'julia@outlook.com','02102788182','I am here to search for a research project partner','13 north street dunedin', 'Information Science', false);
+values (3345345, 'Julia', 'McDowell','17/05/1999','Female', 'jmcdowell', 'password', 'julia@student.otago.ac.nz','(+64)021 0278 8182','I am smart, send me a message!','13 North Street, Dunedin', 'Information Science', false);
 insert into Student (studentID, firstname, surname, dateOfBirth, gender, username, password, email,phoneNumber,description,address, category, searching)  
-values (4866788, 'Cedric', 'Stephani','19/10/1998','male', 'cstephani', 'password', 'cedric@outlook.com','02102789143','I am here to search for a research project partner','44 forth street dunedin', 'Computer Science', false);
+values (4866788, 'Cedric', 'Stephani','19/10/1998','Male', 'cstephani', 'password', 'cedric.wanaka@gmail.com','(+64)021 0278 9143','Please message me if you are looking for student taking Computer Science for post-grad.','23 Forth Street, Dunedin', 'Computer Science', false);
 insert into Student (studentID, firstname, surname, dateOfBirth, gender, username, password, email,phoneNumber,description,address, category, searching)  
-values (2422335, 'Carly', 'Ryan','7/09/1998','female', 'cryan', 'password', 'cryin@outlook.com','021027156881','I am here to search for a research project partner','134 south street dunedin', 'Information Science', false);
+values (2422335, 'Carly', 'Ryan','7/09/1998','Female', 'carlyryan', 'password', 'carlyryan@outlook.com','(+64)021 0271 56881','I am a hard-working student, who is interested in completing a PhD in Information Science.','123 Four Street, Dunedin', 'Information Science', false);
 
 insert into Staff (firstname, surname,dateOfBirth,gender, username, password, email,phoneNumber,description,address, category, searching) 
-values ('Daniel', 'De La Costa','20/03/1990','male', 'dannyboy', 'password', 'dcosta@outlook.com','02102614628','I am here to search for a research project partner','31 grove street dunedin', 'Information Science', false);
+values ('Cathie', 'Williams','20/03/1990','Female', 'imdaniel', 'password', 'dcosta@outlook.com','(+64)021 0261 4628','I am providing an opportunity for an Information Science student to undero research for their PhD or Masters.','31 Grove Street, Dunedin', 'Information Science', false);
+insert into Staff (firstname, surname,dateOfBirth,gender, username, password, email,phoneNumber,description,address, category, searching) 
+values ('Caitlin', 'Owen','20/03/1990','Female', 'imdaniel', 'password', 'dcosta@outlook.com','(+64)021 0261 4628','I am providing an opportunity for an Information Science student to undero research for their PhD or Masters.','31 Grove Street, Dunedin', 'Information Science', false);
+insert into Staff (firstname, surname,dateOfBirth,gender, username, password, email,phoneNumber,description,address, category, searching) 
+values ('Dione', 'Barratt','20/03/1990','Female', 'imdaniel', 'password', 'dcosta@outlook.com','(+64)021 0261 4628','I am providing an opportunity for an Information Science student to undero research for their PhD or Masters.','31 Grove Street, Dunedin', 'Information Science', false);
+insert into Staff (firstname, surname,dateOfBirth,gender, username, password, email,phoneNumber,description,address, category, searching) 
+values ('Mark', 'George','20/03/1990','Male', 'mgeorge', 'password', 'dcosta@outlook.com','(+64)021 0261 4628','I am providing an opportunity for an Information Science student to undero research for their PhD or Masters.','31 Grove Street, Dunedin', 'Information Science', false);
+insert into Staff (firstname, surname,dateOfBirth,gender, username, password, email,phoneNumber,description,address, category, searching) 
+values ('Daniel', 'Alencar da Costa','20/03/1990','Male', 'imdaniel', 'password', 'dcosta@outlook.com','(+64)021 0261 4628','I am providing an opportunity for an Information Science student to undero research for their PhD or Masters.','31 Grove Street, Dunedin', 'Information Science', false);
 insert into Staff (firstname, surname,dateOfBirth,gender, username, password, email,phoneNumber,description,address, category, searching)
-values ('Biology', 'Teacher','10/09/1970','male', 'bioguy69420', 'password', 'PetriDish@outlook.com','02102790163','I am here to search for a research project partner','130 smith street dunedin', 'Biology', false);
+values ('Andrew', 'Trotman','10/09/1970','Male', 'andrewtrotman', 'password', 'andrew@cs.otago.ac.nz','(+64)021 0279 0163','I am here to search for a research project partner','130 Smith Street, dunedin', 'Biology', false);
 insert into Staff (firstname, surname,dateOfBirth,gender, username, password, email,phoneNumber,description,address, category, searching)
-values ('Mark', 'Eting','09/12/1980','male', 'mrmarketing', 'password', 'mark@outlook.com','021026143761','I am here to search for a research project partner','100 smooth street dunedin', 'Marketing', false);
+values ('Steven', 'Livingstone','09/12/1980','Male', 'slivingstone', 'password', 's.livingstone@otago.com','(+64)021 0261 3761','I am here to search for a research project partner','100 smooth street dunedin', 'Marketing', false);
 insert into Staff (firstname, surname,dateOfBirth,gender, username, password, email,phoneNumber,description,address, category, searching)
 values ('Sanjay', 'Gupta','01/01/1978','male', 'sanjaygupta', 'password', 'sajay@gmail.com','02102563721','I am here to search for a research project partner','60 baldwin street dunedin', 'Medical Studies', false);
 insert into Staff (firstname, surname,dateOfBirth,gender, username, password, email,phoneNumber,description,address, category, searching)
